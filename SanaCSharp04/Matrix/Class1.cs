733f06322a351a910480d9fd3878dbbf7ceefdad
Dmytro Burylko <sana22_bdv@student.ztu.edu.ua>
2023-01-18 20:36:30 +0200

    task is done

---
+++ SanaCSharp04/Matrix/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class MatrixMethods
+    {
+        public static int[,] FillMatrix(int rows, int columns)
+        {
+            int[,] matrix = new int[rows, columns];
+            Random random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(-1, 6);
+                }
+            }
+            return matrix;
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j],10}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
