b2a530716b0ac1732e616281c3b4402d16a724f0
Andrii vovk <sana22_vam@student.ztu.edu.ua>
2023-01-19 14:06:31 +0200

    finished task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Matrix
+{
+    internal class MatriXCl
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Уведіть розміри матриці NxM");
+            Console.Write("Уведіть значення m :");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Уведіть значення n :");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, m];
+            var ramdom = new Random();
+            Console.WriteLine("Початкова матриця:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = ramdom.Next(-2, 10);
+                    Console.Write($"{matrix[i, j],-5}");
+
+                }
+                Console.WriteLine();
+            }
+           
